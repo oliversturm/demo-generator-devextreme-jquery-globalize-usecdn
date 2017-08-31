49dbc07103ffbc08f78829cd99e9f524763813c3
Oliver Sturm <oliver@oliversturm.com>
2017-08-31 12:09:41 +0100

    initial checkin

---
+++ index.js
@@ -0,0 +1,55 @@
+const locales = ['en', 'de', 'ja', 'en-GB'];
+
+$.when
+  .apply(
+    $,
+    [].concat
+      .apply(
+        [
+          // standard cldr files
+          'https://unpkg.com/cldr-core/supplemental/likelySubtags.json',
+          'https://unpkg.com/cldr-core/supplemental/timeData.json',
+          'https://unpkg.com/cldr-core/supplemental/weekData.json',
+          'https://unpkg.com/cldr-core/supplemental/currencyData.json',
+          'https://unpkg.com/cldr-core/supplemental/numberingSystems.json'
+        ],
+        // additional files per locale
+        locales.map(l => [
+          'https://unpkg.com/cldr-dates-full/main/' + l + '/ca-gregorian.json',
+          'https://unpkg.com/cldr-numbers-full/main/' + l + '/numbers.json',
+          'https://unpkg.com/cldr-numbers-full/main/' + l + '/currencies.json'
+        ])
+      )
+      .map(s => $.getJSON(s))
+  )
+  .then((...list) => list.map(r => r[0]))
+  .then(Globalize.load)
+  .then(() => {
+    const shorten = l => l.replace(/-.*$/, '');
+
+    // Setting locale depending on browser settings, taking into account
+    // available locales.
+    const requestedLocale = navigator.language || navigator.browserLanguage;
+    const setLocale =
+      locales.find(i => i === requestedLocale) ||
+      locales.find(i => i === shorten(requestedLocale)) ||
+      'en';
+    DevExpress.localization.locale(setLocale);
+
+    // Alternatively, set a specific locale:
+    // DevExpress.localization.locale('de');
+
+    // Now we can set up some widgets:
+    $('#button').dxButton({
+      width: '200px',
+      text: "Say 'Hello world'",
+      onClick: function() {
+        DevExpress.ui.dialog.alert('Hello world!', '', false);
+      }
+    });
+
+    $('#datebox').dxDateBox({
+      width: '200px',
+      placeholder: 'Please enter a date'
+    });
+  });
